9fdac9ac0ac4457fb6d1010bab971e281acbbb16
dm201558 <dm201558@gmail.com>
2026-01-26 21:18:21 +0530

    first commit

---
+++ src/test/java/com/dheerajmehra/hospitality_service/HospitalityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dheerajmehra.hospitality_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
